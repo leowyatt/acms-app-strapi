ca3059eaa912d936b05a81a496707fc8b89954e7
leowyatt <leowyatt@yandex.ru>
2023-02-14 21:18:50 +0300

    init default

---
+++ src/admin/App.jsx
@@ -0,0 +1,15 @@
+// in src/admin/App.jsx
+import * as React from "react";
+import { Admin, Resource, ListGuesser } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={ListGuesser} />
+    <Resource name="comments" list={ListGuesser} />
+  </Admin>
+);
+
+export default App;
